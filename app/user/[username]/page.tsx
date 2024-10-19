9e679c7d32c71a14b6e17a0742bbf8effc3cc752
Alao Zubair Lanre <alaozubair@gmail.com>
2024-10-19 08:45:57 +0100

    add error catching user not found

--- app/user/[username]/page.tsx
+++ app/user/[username]/page.tsx
@@ -3,9 +3,9 @@ import UserProfile from '@/components/UserProfile'
 import { fetchGitHubRepos, fetchGitHubUsername } from '@/lib/actions/Api';
 import { columns } from '@/components/repoTable/columns';
 import { DataTable } from '@/components/repoTable/DataTable';
-import SearchForm from '@/components/forms/SearchForm';
 import Image from 'next/image';
 import Link from 'next/link';
+import Search from '@/components/Search';
 
 export interface RepoProps {
   name: string;
@@ -16,39 +16,50 @@ export interface RepoProps {
 }
 
 const User = async ({params: { username }}: SearchParamProps  ) => {
-  const user =  await fetchGitHubUsername(username);
-  const repos = await fetchGitHubRepos(username) 
-  
-  return (
-    <div className='w-full'>
-      <div className="">
-        <Link href="/" >
-          <Image
-            src="/assets/icons/arrow.svg"
-            height={50}
-            width={50}
-            alt="arrow-icon"
-            className="w-[30px] h-fit mx-8 my-4"
-          />
-        </Link>
-        {/* <SearchForm /> */}
-      </div>
-      <div className='lg:flex  gap-4'>
-        <UserProfile 
-          username={user.login}
-          name={user.name}
-          avatarUrl={user.avatar_url}
-          location={user.location || "No location available"}
-          bio = {user.bio || "No bio available"}
-          publicRepos={user.public_repos}
-        />
+  try {
+    const user = await fetchGitHubUsername(username);
+    const repos = await fetchGitHubRepos(username);
 
-        <div className='w-full lg:w-3/4 mt-8 lg:mt-0 px-8'>
-          <DataTable columns={columns} data={repos} />
+    return (
+      <div className='w-full'>
+        <div className="">
+          <Link href="/" >
+            <Image
+              src="/assets/icons/arrow.svg"
+              height={50}
+              width={50}
+              alt="arrow-icon"
+              className="w-[30px] h-fit mx-8 my-4"
+            />
+          </Link>
+        </div>
+
+        <div className='lg:flex gap-4'>
+          <UserProfile 
+            username={user.login}
+            name={user.name}
+            avatarUrl={user.avatar_url}
+            location={user.location || "No location available"}
+            bio={user.bio || "No bio available"}
+            publicRepos={user.public_repos}
+          />
+
+          <div className='w-full lg:w-3/4 mt-8 lg:mt-0 px-8'>
+            <DataTable columns={columns} data={repos} />
+          </div>
         </div>
       </div>
-    </div>
-  )
+    );
+
+  } catch (error) {
+    return (
+      <div className='w-full text-center flex justify-center items-center flex-col min-h-screen'>
+        <h1 className="text-xl font-bold">Something went wrong</h1>
+        <p className="mt-2 text-red-500">{error instanceof Error && error.message}</p>
+        <Search />
+      </div>
+    );
+  }
 }
 
-export default User
+export default User;
